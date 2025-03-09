6a7e444d2679b90d96c967a9d66e33bdff277beb
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2025-03-10 00:17:07 +0800

    modified:change-text-link-footer

--- src/app/components/footer/component/LinkMediaSocialContact.tsx
+++ src/app/components/footer/component/LinkMediaSocialContact.tsx
@@ -7,13 +7,13 @@ const LinkMediaSocialContact = () => {
         <span className=" w-[0.938rem] h-[0.938rem] bg-white rounded-[100%]"></span>
         CONTACT ME
       </Link>
-      <Link href={"#"} className=" text-black  h-[3.6rem] flex text-[1.6rem] rounded-[0.375rem] text-center items-center justify-center gap-[1rem]">
+      <Link href={"https://www.instagram.com/xlennzii/"} className=" text-black  h-[3.6rem] flex text-[1.6rem] rounded-[0.375rem] text-center items-center justify-center gap-[1rem]">
         <span className=" w-[0.938rem] h-[0.938rem] bg-black rounded-[100%]"></span>
-        CONTACT ME
+        Instagram
       </Link>
       <Link href={"#"} className=" text-black w-[15rem] h-[3.6rem] flex  text-[1.6rem] rounded-[0.375rem] text-center items-center justify-center gap-[1rem]">
         <span className=" w-[0.938rem] h-[0.938rem] bg-black rounded-[100%]"></span>
-        CONTACT ME
+        Linkedin
       </Link>
     </div>
   );
