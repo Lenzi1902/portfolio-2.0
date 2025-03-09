cfaf6b0c32b0985f5ea7e3e224de775d530f780e
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2025-03-09 22:09:17 +0800

    modified:landing component

--- src/app/components/landing/Landing.component.tsx
+++ src/app/components/landing/Landing.component.tsx
@@ -1,7 +1,14 @@
+import TopLanding from "./component/TopLanding";
+
 const LandingComponent = () => {
   return (
-    <div className="min-h-screen  max-w-screen bg-[url('/BackgroundLandingComponent.png')]">
-      <div>Landing Components</div>
+    <div className="min-h-[110vh]  max-w-screen bg-[url('/BackgroundLandingComponent.png')] text-white px-[2rem] py-[2rem] flex flex-col gap-[5rem]">
+      <TopLanding />
+      <div className=" text-[6rem]">
+        <h1>A FRONTEND AND </h1>
+        <h1>BACKEND WEB </h1>
+        <h1>DEVELOPERS</h1>
+      </div>
     </div>
   );
 };
---
+++ src/app/components/landing/component/TopLanding.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+
+const TopLanding = () => {
+  return (
+    <div className="max-w-screen flex justify-between items-center text-[2rem] font-inter">
+      <h1>AGUNG GEDE</h1>
+      <img src={"./logo.png"} alt={""} className=" w-[2.8rem]" />
+    </div>
+  );
+};
+
+export default TopLanding;
