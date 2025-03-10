d129bd69b475c17639f44cb10ebebd9806e36574
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2025-03-10 23:59:39 +0800

    modified:adding-animation

--- src/app/components/about/About.component.tsx
+++ src/app/components/about/About.component.tsx
@@ -1,10 +1,47 @@
+"use client";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import Lenis from "lenis";
+import { useEffect } from "react";
+import SplitType from "split-type";
+
 const AboutComponent = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const splitTypes = document.querySelectorAll(".reveal-text");
+    splitTypes.forEach((char) => {
+      const text = new SplitType(char as HTMLElement, { types: "chars,words" });
+
+      gsap.from(text.chars, {
+        scrollTrigger: {
+          trigger: char,
+          start: "top 80%",
+          end: "top 20%",
+          scrub: true,
+        },
+        opacity: 0.2,
+        stagger: 0.1,
+      });
+    });
+
+    const lenis = new Lenis();
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return (
-    <div className="min-h-[110vh] max-w-screen px-[5rem] py-[3rem] flex flex-col gap-[3rem]">
-      <h1 className=" font-inter font-extrabold text-[4.313rem]">Nice to meet you all.</h1>
+    <div className="min-h-[140vh] max-w-screen px-[5rem] py-[3rem] flex flex-col gap-[3rem]">
+      <h1 className=" font-inter font-extrabold text-[4.313rem]  reveal-text">Nice to meet you all.</h1>
       <div className=" text-[2rem] flex flex-col gap-[2rem] w-[90%]">
-        <p>Hi, I&apos;m Anak Agung, a passionate Full-Stack Developer with a strong interest in building scalable and efficient web applications. Currently, I&apos;m pursuing a degree in Information Systems, where I&apos;m deepening my knowledge of software development, databases, and system architecture.</p>
-        <p>
+        <p className="reveal-text">Hi, I&apos;m Anak Agung, a passionate Full-Stack Developer with a strong interest in building scalable and efficient web applications. Currently, I&apos;m pursuing a degree in Information Systems, where I&apos;m deepening my knowledge of software development, databases, and system architecture.</p>
+        <p className="reveal-text">
           With experience in both front-end and back-end technologies, I enjoy crafting seamless user experiences while ensuring robust and optimized server-side performance. I&apos;m always eager to learn new technologies and stay up-to-date with industry trends to improve my skills and create innovative solutions. Let&apos;s build something
           amazing together!
         </p>
--- src/app/components/projects/Projects.component.tsx
+++ src/app/components/projects/Projects.component.tsx
@@ -1,10 +1,48 @@
+"use client";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import Lenis from "lenis";
+import { useEffect } from "react";
+import SplitType from "split-type";
+
 const ProjectsComponent = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const splitTypes = document.querySelectorAll(".reveal-text-projects");
+    splitTypes.forEach((char) => {
+      const text = new SplitType(char as HTMLElement, { types: "chars,words" });
+
+      gsap.from(text.chars, {
+        scrollTrigger: {
+          trigger: char,
+          start: "top 80%",
+          end: "top 20%",
+          scrub: true,
+        },
+        scaleY: 0,
+        y: -20,
+        transformOrigin: "top",
+        stagger: 0.1,
+      });
+    });
+
+    const lenis = new Lenis();
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <div className="min-h-screen max-w-screen">
       <div className=" flex justify-center">
         <div className=" w-[83%] leading-tight">
-          <h1 className=" font-black text-[9.375rem] text-center">LET’S DO THIS TOGETHER</h1>
-          <p className=" text-[2rem] text-center">We believe that the best work comes from collaboration. Understanding you deeply. Sharing your vision and committing to your future. If that’s the kind of relationship you’re looking for, let’s talk.</p>
+          <h1 className=" font-black text-[9.375rem] text-center reveal-text-projects">LET’S DO THIS TOGETHER</h1>
+          <p className=" text-[2rem] text-center reveal-text">We believe that the best work comes from collaboration. Understanding you deeply. Sharing your vision and committing to your future. If that’s the kind of relationship you’re looking for, let’s talk.</p>
         </div>
       </div>
     </div>
