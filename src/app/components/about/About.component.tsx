1aad2d67d00ac51b65f890c6e3539fc9b1a74cd0
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2025-04-05 02:12:13 +0800

    add:adding-foto-and-update-about-component

--- src/app/components/about/About.component.tsx
+++ src/app/components/about/About.component.tsx
@@ -37,14 +37,19 @@ const AboutComponent = () => {
     requestAnimationFrame(raf);
   }, []);
   return (
-    <section className="min-h-[140vh] max-w-screen px-[5rem] py-[3rem] flex flex-col gap-[3rem]" id="section-about" wb-section="about">
-      <h1 className=" font-inter font-extrabold text-[4.313rem]  reveal-text">Nice to meet you all.</h1>
-      <div className=" text-[2rem] flex flex-col gap-[2rem] w-[90%]">
-        <p className="reveal-text">Hi, I&apos;m Anak Agung, a passionate Full-Stack Developer with a strong interest in building scalable and efficient web applications. Currently, I&apos;m pursuing a degree in Information Systems, where I&apos;m deepening my knowledge of software development, databases, and system architecture.</p>
-        <p className="reveal-text">
-          With experience in both front-end and back-end technologies, I enjoy crafting seamless user experiences while ensuring robust and optimized server-side performance. I&apos;m always eager to learn new technologies and stay up-to-date with industry trends to improve my skills and create innovative solutions. Let&apos;s build something
-          amazing together!
-        </p>
+    <section className="min-h-[140vh] max-w-screen px-[5rem] py-[3rem] flex gap-[2rem] items-center" id="section-about" wb-section="about">
+      <div className=" w-[120rem]">
+        <img src="./sketsa-foto-portfolio.png" alt="" className=" rounded-xl" />
+      </div>
+      <div className="flex flex-col gap-[2rem]">
+        <h1 className=" font-inter font-extrabold text-[3.33rem]  reveal-text">Nice to meet you all.</h1>
+        <div className=" text-[1.8rem] flex flex-col gap-[2rem] w-[90%]">
+          <p className="reveal-text">Hi, I&apos;m Anak Agung, a passionate Full-Stack Developer with a strong interest in building scalable and efficient web applications. Currently, I&apos;m pursuing a degree in Information Systems, where I&apos;m deepening my knowledge of software development, databases, and system architecture.</p>
+          <p className="reveal-text">
+            With experience in both front-end and back-end technologies, I enjoy crafting seamless user experiences while ensuring robust and optimized server-side performance. I&apos;m always eager to learn new technologies and stay up-to-date with industry trends to improve my skills and create innovative solutions. Let&apos;s build something
+            amazing together!
+          </p>
+        </div>
       </div>
     </section>
   );
