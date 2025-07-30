a533e3c05edd4dbd219a5c605d05e76f9cd27afc
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2025-07-30 08:02:43 +0800

    modified:all reponsive mobile in about section

--- src/app/components/about/About.component.tsx
+++ src/app/components/about/About.component.tsx
@@ -1,22 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
+import { ScrollTrigger, SplitText } from "gsap/all";
 import Lenis from "lenis";
 import { useEffect } from "react";
-import SplitType from "split-type";
 
 const AboutComponent = () => {
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger, SplitText);
 
-    const splitTypes = document.querySelectorAll(".reveal-text");
-    splitTypes.forEach((char) => {
-      const text = new SplitType(char as HTMLElement, { types: "chars,words" });
+    document.querySelectorAll(".reveal-text").forEach((element) => {
+      const split = SplitText.create(element, {
+        type: "chars",
+      });
 
-      gsap.from(text.chars, {
+      gsap.from(split.chars, {
         scrollTrigger: {
-          trigger: char,
+          trigger: element,
           start: "top 80%",
           end: "top 20%",
           scrub: true,
@@ -37,18 +38,16 @@ const AboutComponent = () => {
     requestAnimationFrame(raf);
   }, []);
   return (
-    <section className="min-h-[140vh] max-w-screen px-[5rem] py-[3rem] flex gap-[2rem] items-center" id="section-about" wb-section="about">
-      <div className=" w-[120rem]">
-        <img src="./sketsa-foto-portfolio.png" alt="" className=" rounded-xl" />
+    <section className="min-h-[140vh] max-w-screen px-[0.2rem] md:px-[5rem] py-[5rem] md:py-[10rem] grid grid-cols-1 grid-rows-10 md:grid-cols-5 md:grid-rows-5 gap-[1rem] md:gap-[2rem] " id="section-about" wb-section="about">
+      <div className=" row-span-4 md:col-span-2 md:row-span-5 ">
+        <img src="./sketsa-foto-portfolio.png" alt="" className=" rounded-xl w-[100%]" />
       </div>
-      <div className="flex flex-col gap-[2rem]">
-        <h1 className=" font-inter font-extrabold text-[3.33rem]  reveal-text">Nice to meet you all.</h1>
-        <div className=" text-[1.8rem] flex flex-col gap-[2rem] w-[90%]">
-          <p className="reveal-text">Hi, I&apos;m Anak Agung, a passionate Full-Stack Developer with a strong interest in building scalable and efficient web applications. Currently, I&apos;m pursuing a degree in Information Systems, where I&apos;m deepening my knowledge of software development, databases, and system architecture.</p>
-          <p className="reveal-text">
-            With experience in both front-end and back-end technologies, I enjoy crafting seamless user experiences while ensuring robust and optimized server-side performance. I&apos;m always eager to learn new technologies and stay up-to-date with industry trends to improve my skills and create innovative solutions. Let&apos;s build something
-            amazing together!
-          </p>
+      <div className="flex flex-col gap-[1rem] md:gap-[2rem] row-span-6 row-start-5 md:col-span-3 md:row-span-5 md:col-start-3 w-[99%]">
+        <h1 className=" font-inter font-extrabold text-[1.1rem] md:text-[3.33rem]  reveal-text">Nice to meet you all.</h1>
+        <div className=" text-[1rem] md:text-[1.8rem] flex flex-col ap-[0.2rem] md:gap-[1rem] ">
+          <p className="reveal-text">I&apos;m Anak Agung, a Full-Stack Developer passionate about building fast, scalable web apps.</p>
+          <p className="reveal-text">Currently studying Information Systems, I enjoy working on both front-end and back-end crafting smooth user experiences and robust server logic.</p>
+          <p className="reveal-text">Always curious, always learning especially new tools and tech to build better solutions.</p>
         </div>
       </div>
     </section>
