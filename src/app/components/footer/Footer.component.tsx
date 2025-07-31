ce9dd1cfa614a33fed9cb1c8671c6766fc92a2ac
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2025-07-31 12:39:03 +0800

    modified: all responsive footer component teks and layout

--- src/app/components/footer/Footer.component.tsx
+++ src/app/components/footer/Footer.component.tsx
@@ -2,11 +2,11 @@ import LinkMediaSocialContact from "./component/LinkMediaSocialContact";
 
 const FooterComponent = () => {
   return (
-    <div className="min-h-[33.75rem] max-w-screen flex items-center">
+    <div className="max-h-[33.75rem] max-w-screen flex items-center mt-[10rem] md:mt-[20rem] ">
       <div className="  font-bold">
-        <h1 className="text-[6rem]">LET’S</h1>
-        <div className=" flex items-center gap-[6rem]">
-          <h1 className="text-[6rem]">CONNECT</h1>
+        <h1 className="text-[3rem] md:text-[6rem]">LET’S</h1>
+        <div className=" flex flex-col md:flex-row md:items-center gap-[1rem] md:gap-[6rem] ">
+          <h1 className="text-[3rem] md:text-[6rem]">CONNECT</h1>
           <LinkMediaSocialContact />
         </div>
       </div>
--- src/app/components/footer/component/LinkMediaSocialContact.tsx
+++ src/app/components/footer/component/LinkMediaSocialContact.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const LinkMediaSocialContact = () => {
   return (
-    <div className=" flex gap-[4rem]">
+    <div className=" flex flex-col md:flex-row gap-[4rem]">
       <GsapMagnetic>
         <TranisitionLink href="/contact" label="CONTACT ME" className=" bg-contactme w-[15rem] h-[3.6rem] flex text-white text-[1.6rem] rounded-[0.375rem] text-center items-center justify-center gap-[1rem]">
           <span className=" w-[0.938rem] h-[0.938rem] bg-white rounded-[100%]"></span>
@@ -16,13 +16,13 @@ const LinkMediaSocialContact = () => {
         </Link> */}
       </GsapMagnetic>
       <GsapMagnetic>
-        <Link href={"https://www.instagram.com/xlennzii/"} className=" text-black  h-[3.6rem] flex text-[1.6rem] rounded-[0.375rem] text-center items-center justify-center gap-[1rem]">
+        <Link href={"https://www.instagram.com/xlennzii/"} className=" text-black  h-[3.6rem] hidden md:flex text-[1.6rem] rounded-[0.375rem] text-center items-center justify-center gap-[1rem]">
           <span className=" w-[0.938rem] h-[0.938rem] bg-black rounded-[100%]"></span>
           Instagram
         </Link>
       </GsapMagnetic>
       <GsapMagnetic>
-        <Link href={"#"} className=" text-black w-[15rem] h-[3.6rem] flex  text-[1.6rem] rounded-[0.375rem] text-center items-center justify-center gap-[1rem]">
+        <Link href={"#"} className=" text-black w-[15rem] h-[3.6rem] hidden md:flex  text-[1.6rem] rounded-[0.375rem] text-center items-center justify-center gap-[1rem]">
           <span className=" w-[0.938rem] h-[0.938rem] bg-black rounded-[100%]"></span>
           Linkedin
         </Link>
